6305561f2774ad6b5eeb395d4308e1ec8a1ea4c3
alaaabuobaid <146891074+alaaabuobaid@users.noreply.github.com>
2024-05-26 05:31:07 -0700

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,10 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+  
+    const contactForm = document.getElementById('contact-form');
+    contactForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        alert('Your message has been sent!');
+        contactForm.reset();
+    });
+});
